43003bbab8c4f7bcefb9d6867c6ab626d02e6cce
wcj <344259261@qq.com>
2018-07-11 17:54:14 +0800

    增加store

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store';
 import axios from './utils/axios';
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/page/Cinema.vue
+++ src/page/Cinema.vue
@@ -4,7 +4,10 @@
 
 <script>
 export default {
-  name: "cinema"
+  name: "cinema",
+  mounted() {
+    console.log(this.$store.state)
+  }
 };
 </script>
 
